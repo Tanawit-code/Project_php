e34be1b4ee2f3f7033074acbb283b18315f37ec9
Tanawit-code <freshjk24@gmail.com>
2025-09-26 14:21:35 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $query = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($query);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+        header("Location: index.php");
+        exit;
+    } else {
+        echo "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
+    }
+}
+?>
+<form method="post">
+    ชื่อผู้ใช้: <input type="text" name="username" required><br>
+    รหัสผ่าน: <input type="password" name="password" required><br>
+    <button type="submit">เข้าสู่ระบบ</button>
+</form>
